74807ec1a91e816d75a4661751a8bc05190e4306
Cosmin Veștemean <cosmin.ve@gmail.com>
2023-10-12 17:44:28 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,6 +1,6 @@
 console.log('Client.js loaded')
 
-const socket = io('http://localhost:5000')
+const socket = io('https://retailers-ac9953f6caca.herokuapp.com:5000')
 const client = feathers()
 const socketClient = feathers.socketio(socket)
 
