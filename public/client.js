6c9ec2d427f568274e1680f9288f263a767dc2c6
Cosmin Veștemean <cosmin.ve@gmail.com>
2023-10-17 16:14:50 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1541,7 +1541,15 @@ function displayDocsForRetailers(result, trdr, sosource, fprms, series) {
     button2.onclick = async function () {
       //await createXML(row.findoc, trdr, sosource, fprms, series)
       var domObj = await cheatGetXmlFromS1(row.findoc)
-      alert('Trimis:' + domObj.trimis + '\n' + 'XML:' + domObj.dom)
+      //add cell and textarea
+      var textarea = document.createElement('textarea')
+      textarea.className = 'textarea is-small'
+      textarea.rows = 10
+      textarea.cols = 50
+      textarea.innerHTML = domObj.xml
+      //add cell
+      var td = tr.insertCell()
+      td.appendChild(textarea)
     }
     actions.appendChild(button2)
     //save xml button
