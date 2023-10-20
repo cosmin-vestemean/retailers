e3b59ab6c6eb13591a72a42bdab0471b5fa9eed2
Cosmin Veștemean <cosmin.ve@gmail.com>
2023-10-20 13:14:23 +0300

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1543,6 +1543,8 @@ function displayDocsForRetailers(result, trdr, sosource, fprms, series) {
     button.className = 'button is-small is-primary'
     button.innerHTML = 'Send Invoice'
     button.onclick = async function () {
+      //update btn caption to sending
+      button.innerHTML = 'Sending...'
       await sendInvoice(row.findoc)
       .then(async (response) => {
       console.log('response', response)
@@ -1570,13 +1572,15 @@ function displayDocsForRetailers(result, trdr, sosource, fprms, series) {
       body['KEY'] = row.findoc
       body['DATA'] = {}
       body['DATA']['MTRDOC'] = [{"CCCXMLSendDate": new Date().toISOString().slice(0, 19).replace('T', ' ')}]
-      client
+      await client
             .service('setDocument')
             .create(body)
             .then((res) => {
               console.log(res)
             })
           })
+      //update btn caption to sent
+      button.innerHTML = 'Sent'
     }
     actions.appendChild(button)
     //create xml button
