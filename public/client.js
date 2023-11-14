4d166459ec075756ed2ae6419412374652be89c1
Cosmin Veștemean <cosmin.ve@gmail.com>
2023-11-14 20:01:22 +0200

    Update client.js

--- public/client.js
+++ public/client.js
@@ -2255,7 +2255,8 @@ async function createXML(findoc, trdr, sosource, fprms, series) {
   //header
   var _HEADER = []
   CCCXMLS1MAPPINGS_HEADER.forEach(async (item) => {
-    if (trim(item.SQL) == '') {
+    item.SQL = trim(item.SQL)
+    if (item.SQL == '') {
       var o = {}
       o.xmlNode = item.XMLNODE
       o.table1 = item.S1TABLE1
