74c5d8081520011c0c9e1b3ddd044bd7dd86fd9f
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-27 15:11:32 +0200

    Update client.js and index.html

--- public/client.js
+++ public/client.js
@@ -1,20 +1,11 @@
 console.log('Client.js loaded')
 
-/* //const socket = io('https://retailers-ac9953f6caca.herokuapp.com')
+//const socket = io('https://retailers-ac9953f6caca.herokuapp.com')
 const socket = io('www.retailers.acct.ro')
 const client = feathers()
 const socketClient = feathers.socketio(socket)
 
-client.configure(socketClient) */
-
-import socketio from '@feathersjs/socketio-client'
-import io from 'socket.io-client'
-import {createClient} from '..src/client.js'
-
-const socket = io('www.retailers.acct.ro')
-
-const client = createClient(socketio(socket))
-
+client.configure(socketClient)
 
 client.use('sftp', socketClient.service('sftp'), {
   methods: ['downloadXml', 'storeXmlInDB', 'uploadXml'],
