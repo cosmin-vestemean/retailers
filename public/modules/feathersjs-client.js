e8ad78dfba71571b8e08ead4dfacbf64919e4072
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-27 15:59:07 +0200

    Add socket.io-client and @feathersjs/feathers dependencies

--- public/modules/feathersjs-client.js
+++ public/modules/feathersjs-client.js
@@ -1,3 +1,7 @@
+import io from 'socket.io-client'
+import { feathers } from '@feathersjs/feathers'
+
+
 const socket = io('www.retailers.acct.ro');
 const client = feathers();
 const socketClient = feathers.socketio(socket);
