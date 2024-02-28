1012dc34ccce29cf41f3b946bec27b5609a3c2e9
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-28 15:03:02 +0200

    create retailer.js and start moving related code from client.js

--- public/client.js
+++ public/client.js
@@ -64,40 +64,6 @@ var url = '',
   privateKey = '',
   fingerprint = ''
 
-async function getRetailerConfData() {
-  var localStorageRetailer
-  try {
-    localStorageRetailer = parseInt(localStorage.getItem('trdr_retailer'))
-  } catch (err) {
-    console.log(err)
-  }
-  client
-    .service('CCCSFTP')
-    .find({
-      query: {
-        TRDR_RETAILER: localStorageRetailer
-      }
-    })
-    .then((res) => {
-      console.log(res)
-      //URL
-      document.getElementById('URL').value = res.data[0].URL
-      //PORT
-      document.getElementById('PORT').value = res.data[0].PORT
-      //USERNAME
-      document.getElementById('USERNAME').value = res.data[0].USERNAME
-      //PASSPHRASE
-      document.getElementById('PASSPHRASE').value = res.data[0].PASSPHRASE
-      privateKey = res.data[0].PRIVATEKEY
-      document.getElementById('FINGERPRINT').value = res.data[0].FINGERPRINT
-      document.getElementById('TRDR_RETAILER').value = res.data[0].TRDR_RETAILER
-      //INITIALDIRIN
-      document.getElementById('INITIALDIRIN').value = res.data[0].INITIALDIRIN
-      //INITIALDIROUT
-      document.getElementById('INITIALDIROUT').value = res.data[0].INITIALDIROUT
-    })
-}
-
 async function setRetailerId(trdr, urlLogo) {
   localStorage.setItem('trdr_retailer', trdr)
   localStorage.setItem('logo_retailer', urlLogo)
@@ -3390,7 +3356,6 @@ async function toggleFacturiNetrimise() {
 }
 
 export {
-  getRetailerConfData,
   setRetailerId,
   getClientConfData,
   openTab,
