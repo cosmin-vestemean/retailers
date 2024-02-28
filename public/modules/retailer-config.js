f81819276dd55ac7ce758a6e4eede96406179306
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-02-28 19:39:00 +0200

    Refactor loadListaDocumente function to accept a parameter

--- public/modules/retailer-config.js
+++ public/modules/retailer-config.js
@@ -2134,7 +2134,7 @@ function getDocument() {
   return currentDoc
 }
 
-export function loadListaDocumente() {
+export function loadListaDocumente(loadMapping) {
   //get all documents from database table CCCDOCUMENTES1MAPPINGS
   var localStorageRetailer
   try {
