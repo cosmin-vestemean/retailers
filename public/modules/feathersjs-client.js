0e6413551fb25ecda54bdd0477e3428c17b08a7b
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-03-07 16:24:03 +0200

    Update socket URL in feathersjs-client.js

--- public/modules/feathersjs-client.js
+++ public/modules/feathersjs-client.js
@@ -1,5 +1,5 @@
-//const socket = io('https://retailers-ac9953f6caca.herokuapp.com')
-const socket = io('https://retailers-modular-975638ebe522.herokuapp.com')
+const socket = io('https://retailers-ac9953f6caca.herokuapp.com')
+//const socket = io('https://retailers-modular-975638ebe522.herokuapp.com')
 ////const socket = io('www.retailers.acct.ro')
 const client = feathers()
 const socketClient = feathers.socketio(socket)
