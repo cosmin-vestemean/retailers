989543484e222c696d53bff63a4e53cff4f3e6b2
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-06-13 16:31:50 +0300

    Refactor retailers.js to update retailer information for Dedeman

--- public/retailers.js
+++ public/retailers.js
@@ -10,7 +10,7 @@ export const retailers = [
     '11322',
     'https://cdn-static.carrefour.ro/unified/assets/images/dist/logo/default/carrefour.png', 'Carrefour'
   ),
-  new Retailer('11654', 'https://cdn.dedeman.ro/static/version1718221031/frontend/Dedeman/white/ro_RO/images/logo.svg', 'Lidl'),
+  new Retailer('11654', 'https://cdn.dedeman.ro/static/version1718221031/frontend/Dedeman/white/ro_RO/images/logo.svg', 'Dedeman'),
 ]
 
 export async function drawRetailers() {
