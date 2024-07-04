9c8b0be2497902216a8c362bd03301646c26998b
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-07-04 15:00:48 +0300

    Refactor conectorEdinet to remove deprecated secureOptions in app.js

--- src/app.js
+++ src/app.js
@@ -1015,8 +1015,6 @@ class conectorEdinet {
           }
           //data/edinet/config > p12 file and cer file
           config.secureOptions = {
-            secureProtocol: 'TLSv1_2_method',
-            secureOptions: 'SSL_OP_NO_SSLv3',
             pfx: fs.readFileSync('data/edinet/config/edinet.p12'),
             passphrase: 'infinite2015',
             ca: fs.readFileSync('data/edinet/config/edinet-edinet-u-86669807 2015-03-18 192254.cer'),
