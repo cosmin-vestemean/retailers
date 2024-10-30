87fbc65e417e9532dfddaf92b0cc7155243d285f
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-10-30 18:10:53 +0200

    Refactor SftpServiceClass in app.js to remove unnecessary console.log statement

--- src/app.js
+++ src/app.js
@@ -597,7 +597,7 @@ class SftpServiceClass {
       return { success: false, errors: errors }
     }
 
-    console.log('jsonOrder', JSON.stringify(jsonOrder))
+    //console.log('jsonOrder', JSON.stringify(jsonOrder))
 
     // Process ITELINES array
     let itelines = jsonOrder['DATA']['ITELINES']
