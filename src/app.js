a01e2561273cc8ff5bb862ead9b5fd99dc4dc896
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-05 15:08:11 +0200

    Refactor variable declarations in SftpServiceClass for improved clarity

--- src/app.js
+++ src/app.js
@@ -436,8 +436,8 @@ class SftpServiceClass {
       CCCSFTPXML: -1,
       MESSAGETEXT: 'Scanning for orders and aperak files...'
     })
-    data = {}
-    params = { query: { retailer: 11639, rootPath: orderPath, startsWith: 'ORDERS_' } }
+    let data = {}
+    let params = { query: { retailer: 11639, rootPath: orderPath, startsWith: 'ORDERS_' } }
     const dwlRes = await this.downloadXml(data, params)
     await app.service('CCCORDERSLOG').create({
       TRDR_CLIENT: 1,
