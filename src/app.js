16be6297aeb7d4a0caa744013a298d65f40ff0f2
Cosmin Veștemean <cosmin.ve@gmail.com>
2024-11-19 16:14:22 +0200

    Fix order ID extraction and update message text in CCCORDERSLOG creation

--- src/app.js
+++ src/app.js
@@ -454,13 +454,13 @@ class SftpServiceClass {
       storeRes[0].message === 'No files inserted'
     ) {
     } else {
-      const orderId = JSON.parse(storeRes.response.JSONDATA).Order.ID || 'storeXmlInDB'
+      const orderId = JSON.parse(storeRes[0].response.JSONDATA).Order.ID || 'storeXmlInDB'
       await app.service('CCCORDERSLOG').create({
         TRDR_CLIENT: 1,
         TRDR_RETAILER: -1,
         ORDERID: orderId,
         CCCSFTPXML: -1,
-        MESSAGETEXT: '<pre><code>' + JSON.stringify(storeRes) + '</code></pre>'
+        MESSAGETEXT: '<pre><code>' + JSON.stringify(storeRes[0].response.message) + '</code></pre>'
       })
     }
     console.log('Creating orders...')
