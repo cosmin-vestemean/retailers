d533b1ff03568befbeda638a29e8297c326e53a5
Cosmin Veștemean <cosmin.ve@gmail.com>
2025-07-25 15:23:55 +0300

    Remove diagnostics service and update retailer file manager to use real IP detection

--- src/services/index.js
+++ src/services/index.js
@@ -12,7 +12,6 @@ import { cccsftpxml } from './CCCSFTPXML/CCCSFTPXML.js'
 
 import { cccsftp } from './CCCSFTP/CCCSFTP.js'
 import { ipLogger } from './ip-logger/ip-logger.service.js'
-import { diagnostics } from './diagnostics/diagnostics.js'
 
 export const services = (app) => {
   app.configure(cccorderslog)
@@ -30,8 +29,6 @@ export const services = (app) => {
   app.configure(cccsftp)
 
   app.configure(ipLogger)
-
-  app.configure(diagnostics)
 
   // All services will be registered here
 }
