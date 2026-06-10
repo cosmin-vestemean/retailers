7040efad2b0305058283f759727b4e29f8acc3e6
Cosmin Vestemean <cosmin.ve@gmail.com>
2026-06-10 11:26:52 +0300

    Update default daysOlder value to 30 in invoice days filter

--- frontend/src/components/invoice-table.js
+++ frontend/src/components/invoice-table.js
@@ -305,7 +305,7 @@ export class InvoiceTable extends LightElement {
         <label class="invoice-days-filter">
           <input type="number" class="form-control form-control-sm invoice-days-input"
                  .value=${String(this.daysOlder)} min="1" max="90"
-                 @change=${(e) => { this.daysOlder = parseInt(e.target.value) || 7; this._page = 1; this.loadInvoices() }} />
+                 @change=${(e) => { this.daysOlder = parseInt(e.target.value) || 30; this._page = 1; this.loadInvoices() }} />
           zile
         </label>
         <button class="btn btn-primary btn-sm" @click=${this._downloadAperaks} ?disabled=${this._loading}>
